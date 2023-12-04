d0004c89bc6fb1713c3439ef162efbabd00903dc
Jason Sturges <jason@jasonsturges.com>
2023-12-04 16:14:20 -0600

    Extras: editor.aerial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
