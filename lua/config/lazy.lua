ec79f300fcce1060d406eb6f2d24bcfc80877b4f
Jason Sturges <jason@jasonsturges.com>
2023-12-04 16:13:16 -0600

    Extras: coding.copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
       colorscheme = "solarized-osaka",
     } },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
