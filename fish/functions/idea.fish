9e8265da7bee772d16f200745ba256137e2815af
Jason Sturges <jason@jasonsturges.com>
2023-12-14 15:17:51 -0600

    Fish functions

---
+++ fish/functions/idea.fish
@@ -0,0 +1,4 @@
+function idea --wraps='open -na "IntelliJ IDEA.app" --args $argv' --description 'alias idea=open -na "IntelliJ IDEA.app" --args $argv'
+  open -na "IntelliJ IDEA.app" --args $argv $argv
+        
+end
