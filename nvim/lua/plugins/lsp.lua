8580fee79e6784374fbab798f743137236cbc86f
Jason Sturges <jason@jasonsturges.com>
2024-09-30 01:18:15 -0500

    Update lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,6 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "css-lsp",
-        "stylua",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-      })
-    end,
+    opts = function(_, opts) end,
   },
 }
